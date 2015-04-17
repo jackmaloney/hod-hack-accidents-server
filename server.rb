43d3c0e24c31f87a14137ee6e099ea1992b0532e
Jonathan Pagel <pagel@pobox.com>
2015-04-17 15:56:33 +0100

    simple-minded get to copy post

--- server.rb
+++ server.rb
@@ -16,11 +16,15 @@ get '/scan' do
   image_list = {}
   Find.find(dir) do |file|
     if file =~ /.jpg$/i
-      shortname = file.split("/").last
-      longitude = EXIFR::JPEG.new(file).gps.longitude
-      latitude = EXIFR::JPEG.new(file).gps.latitude
-      GH = Geocode_helper.new latitude, longitude
-      image_list[file] = {:longitude => longitude, :latitude => latitude, :name => shortname, :info => GH.get_address_data.inspect}
+      begin
+        shortname = file.split("/").last
+        longitude = EXIFR::JPEG.new(file).gps.longitude
+        latitude = EXIFR::JPEG.new(file).gps.latitude
+        GH = Geocode_helper.new latitude, longitude
+        image_list[file] = {:longitude => longitude, :latitude => latitude, :name => shortname, :info => GH.get_address_data.inspect}
+      rescue
+        puts "rescued"
+      end
     end
     puts image_list.inspect
   end
@@ -31,8 +35,14 @@ get '/' do
   content_type :json
   data = {
     :status => "OK",
-    :data => "Thank you for getting at " + Time.now.to_s
+    :timestamp => Time.now.to_s
   }
+  if params["latitude"] and params["longitude"]
+    data[:latitude] = params["latitude"]
+    data[:longitude] = params["longitude"]
+    GH = Geocode_helper.new params["latitude"], params["longitude"]
+    data[:address_data] = GH.get_address_data
+  end
   data.to_json
 end
 
