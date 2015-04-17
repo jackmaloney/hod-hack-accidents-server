270c9b822ebef47c0346c1e54bd8c03ef551132d
Jonathan Pagel <pagel@pobox.com>
2015-04-17 11:34:47 +0100

    refactored helper so address structure is returned to response json

--- server.rb
+++ server.rb
@@ -20,8 +20,8 @@ post '/' do
     :data => "Thank you for posting at " + Time.now.to_s
   }
   if params["latitude"] and params["longitude"]
-    GH = Geocode_helper.new
-    data[:street] = GH.get_street params["latitude"], params["longitude"]
+    GH = Geocode_helper.new params["latitude"], params["longitude"]
+    data[:address_data] = GH.get_address_data
   end
   data.to_json
 end
--- specs/Geocode_spec.rb
+++ specs/Geocode_spec.rb
@@ -2,7 +2,7 @@ require_relative "../lib/geocode_helper"
 
 describe Geocode_helper do
   before :each do
-    @GH = Geocode_helper.new
+    @GH = Geocode_helper.new 51.437406, -0.036607
   end
 
   it "should say hello" do
@@ -10,7 +10,7 @@ describe Geocode_helper do
   end
 
   it "should get a street from a latitude and longitude" do
-    res = @GH.get_street(51.437406, -0.036607)
+    res = @GH.get_street
     expect(res).to eq "Lutwyche Road"
   end
 end
